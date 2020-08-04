68fe72eddb96f47dc070eed6e68fc3100c8cdca3
eurico77 <euricomagalhaes61@gmail.com>
2020-08-04 12:18:21 -0300

    criando as estilizações

--- src/pages/TeatcherList/index.tsx
+++ src/pages/TeatcherList/index.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
+
+import "./styles.css";
 
 function TeatcherList() {
-  return <h1>lista de professores</h1>;
+  return (
+    <div id="page-teacher-list" className=" container ">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="voltar" />
+          </Link>
+          <img src={logoImg} alt="proffy" />
+        </div>
+
+        <div className="header-content">
+          <strong>Esses são os proffys disponoveis</strong>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default TeatcherList;
